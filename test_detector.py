47a90b7fd416dfa648ce718b7b8516078f4b9686
Abaasi <abaasix49@gmail.com>
2025-06-16 16:56:11 +0300

    Remove ignored files from repository while keeping locally

--- test_detector.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append('src')
-
-from environment.element_detector import demo_element_detection
-
-if __name__ == "__main__":
-    demo_element_detection()
--- test_qlearning.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('src')
-
-from agents.q_learning_agent import demo_q_learning
-
-if __name__ == "__main__":
-    demo_q_learning()
